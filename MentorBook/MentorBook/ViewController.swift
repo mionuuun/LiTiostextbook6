ff6e0eea29d4a34cf0d4fadfaad550f1d1e1f75d
mionuuun <41195985+mionuuun@users.noreply.github.com>
2019-08-25 16:58:31 +0900

    first

---
+++ MentorBook/MentorBook/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  MentorBook
+//
+//  Created by Mio Sato on 2019/08/25.
+//  Copyright © 2019 Mio Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var mentorArray: [Mentor] = []
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var courseLabel: UILabel!
+    
+    var index: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        mentorArray.append(Mentor(name: "ながた" , imageName: "nagata.jpg" , course: "iPhone"))
+        mentorArray.append(Mentor(name: "りょう" , imageName: "ryo.jpg" , course: "Unity"))
+        mentorArray.append(Mentor(name: "たいてぃ" , imageName: "taithi.jpg" , course: "WebS,WebD"))
+        
+        setUI()
+    }
+    
+    @IBAction func mae() {
+        index = index - 1
+        setUI()
+    }
+    
+    @IBAction func tugi() {
+        index = index + 1
+        setUI()
+    }
+    
+    func setUI{
+        imageView.image = mentorArray[index].getImage()
+    }
+
+
+}
+
